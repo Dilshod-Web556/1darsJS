17bfb3420e8943d3db973910844bc4e008d14607
Dilshod-Web556 <GorillaEnergy777@Gmail.com>
2024-12-07 17:26:17 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let name = prompt('Ismingizni kiriting');
+console.log(`Ism: ${name}`);
+
+let surname = prompt('Familyangizni kiriting');
+console.log(`Familiya: ${surname}`);
+
+
+let age = +prompt('Yoshingizni kiriting');
+console.log(`Yosh: ${age}`);
+
+let info = prompt('Yoqtirgan faningizni yozing');
+console.log(`Fan: ${info}`);
+
+let unset = prompt('Yashaydigan tumaningizni kiriting');
+console.log(`Yashash manzili: ${unset}`);
+
+
+let seting = ('Malumotlaringizni kiritganingiz uchun tashakkur');
+alert(seting);
